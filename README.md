c257f2dcd62d4b9ad7d71171aae409e0fc43ff4e
Nate Irwin <nate@nateirwin.net>
2013-10-21 22:54:51 -0600

    Add license, work on ArcGIS Server ingest, and some project cleanup

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # arc2osm
 
-A Node.js app that inserts data coming from ArcGIS Server feature and/or map services into an OpenStreetMap database, via the [OSM API](http://wiki.openstreetmap.org/wiki/API_v0.6).
+A Node.js task that queries ArcGIS Server feature and/or map services and synchronizes inserts, updates, and deletes with an OpenStreetMap database via the [OSM API](http://wiki.openstreetmap.org/wiki/API_v0.6).
 
-Still a work-in-progress.
+The National Park Service uses this internally to synchronize our servicewide datasets (roads, trails, buildings, boundaries, etc.) with our OpenStreetMap render database.
+
+This software is still a work-in-progress, and is very low tech right now. It will likely improve in the future. Some ideas on how it might be improved include:
+
+- Switch to streams
+- Hit ArcGIS Server via SOAP rather than REST
+- Utilize the editor tracking feature introduced in ArcGIS Server 10.1
+
+At this point, however, we are only synchronizing fairly small datasets (< 50,000 records) on an infrequent basis (once a day), so low tech works.
 
 ## Installation
 
 1. Install Node.js
-2. Clone repository
-3. cd into repository
-4. Run npm install
+2. `git clone https://github.com/nationalparkservice/arc2osm`
+3. `cd arc2osm`
+4. `npm install`
+5. Update config.json with information about the ArcGIS Server endpoints you want to ingest data from
+6. `node app.js`
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-/* http://esri.github.io/geojson-utils */
+/*
+ * http://esri.github.io/geojson-utils
+ */
 function esriConverter(){function l(b){var a,c,d,h;if(b){a={};if(b.x)a.type="Point",c=[b.x,b.y];else if(b.points)a.type="MultiPoint",c=b.points;else if(b.paths)c=b.paths,1===c.length?(a.type="LineString",c=c[0]):a.type="MultiLineString";else if(b.rings){c=b.rings;d=[];for(b=0;b<c.length;b++){var e=h=c[b],f=0,g=0,l=e.length,k=e[g],m=void 0;for(g;g<l-1;g++)m=e[g+1],f+=(m[0]-k[0])*(m[1]+k[1]),k=m;0<=f?d.push([h]):d[d.length-1].push(h)}1<d.length?(c=d,a.type="MultiPolygon"):(c=d.pop(),a.type="Polygon")}a.coordinates=c}return a}function k(b){var a=null,c,d;if(b&&(a={type:"Feature"},b.geometry&&(a.geometry=l(b.geometry)),b.attributes)){d={};for(c in b.attributes)d[c]=b.attributes[c];a.properties=d}return a}return{toGeoJson:function(b){var a,c,d;if(b)if(b.features)for(a={type:"FeatureCollection",features:[]},c=b.features,b=0;b<c.length;b++)(d=k(c[b]))&&a.features.push(d);else a=b.geometry?k(b):l(b);return a}}}function geoJsonConverter(){function l(a){var c,d;if("Point"===a)c="esriGeometryPoint";else if("MultiPoint"===a)c="esriGeometryMultipoint",d="points";else if("LineString"===a||"MultiLineString"===a)c="esriGeometryPolyline",d="paths";else if("Polygon"===a||"MultiPolygon"===a)c="esriGeometryPolygon",d="rings";return{type:c,geomHolder:d}}function k(a){var c,d,b,e,f;if("GeometryCollection"===a.type)for(d=[a.geometries.shift()],c=l(d[0].type),b=0;b<a.geometries.length;b++){e=c.type;f=a.geometries[b].type;var g=!1;"esriGeometryPoint"!==e&&"esriGeometryMultipoint"!==e||"Point"!==f&&"MultiPoint"!==f?"esriGeometryPolyline"!==e||"LineString"!==f&&"MultiLineString"!==f?"esriGeometryPolygon"!==e||"Polygon"!==f&&"MultiPolygon"!==f||(g=!0):g=!0:g=!0;g&&d.push(a.geometries[b])}else c=l(a.type),d=[a];"esriGeometryPoint"===c.type&&1<d.length&&(c=l("MultiPoint"));a={type:c.type,spatialReference:{wkid:4326}};if("esriGeometryPoint"===c.type)0===d[0].coordinates.length?(a.x=null,a.y=null):(a.x=d[0].coordinates[0],a.y=d[0].coordinates[1]);else for(a[c.geomHolder]=[],b=0;b<d.length;b++){e=d[b];g=f=void 0;if("MultiPoint"===e.type||"MultiLineString"===e.type||"Polygon"===e.type)g=e.coordinates;else if("Point"===e.type||"LineString"===e.type)g=[e.coordinates];else if("MultiPolygon"===e.type)for(g=[],f=0;f<e.coordinates.length;f++)g.push(e.coordinates[f]);f=g;for(e=0;e<f.length;e++)a[c.geomHolder].push(f[e])}return a}function b(a){var c,b,h;if(a&&(c={},a.geometry&&(c.geometry=k(a.geometry)),a.properties)){h={};for(b in a.properties)h[b]=a.properties[b];c.attributes=h}return c}return{toEsri:function(a){var c,d,h;if(a)if("FeatureCollection"===a.type)for(c={features:[]},d=a.features,a=0;a<d.length;a++)(h=b(d[a]))&&c.features.push(h);else c="Feature"===a.type?b(a):k(a);return c}}};
 
 var completed = 0,
@@ -7,13 +9,14 @@ var completed = 0,
     count = config.arcGisServer.length,
     interval,
     request = require('request'),
-    status = require('./status.json');
+    status = {};
 
 function handleResponse(error, response, callback) {
   if (!error && response.statusCode === 200) {
     callback(response);
   } else {
-    console.log(response.statusCode + ': ' + error);
+    console.log('Error: ' + error);
+    completed++;
   }
 }
 function queryPaged(max, objectIds, url, callback) {
@@ -44,9 +47,11 @@ function queryPaged(max, objectIds, url, callback) {
 
 for (var i = 0; i < count; i++) {
   (function() {
-    var arcGisServer = config.arcGisServer[i];
+    var arcGisServer = config.arcGisServer[i],
+        name = arcGisServer.name;
 
-    console.log(arcGisServer.url + '/query?f=json&where=' + (arcGisServer.query ? arcGisServer.query : 'OBJECTID IS NOT NULL') + '&returnIdsOnly=true');
+    status[name] = require('status/' + name + '.json');
+
     request(arcGisServer.url + '/query?f=json&where=' + (arcGisServer.query ? arcGisServer.query : 'OBJECTID IS NOT NULL') + '&returnIdsOnly=true', function(error, response) {
       handleResponse(error, response, function(response) {
         var objectIds = JSON.parse(response.body).objectIds;
@@ -54,9 +59,13 @@ for (var i = 0; i < count; i++) {
         if (objectIds) {
           request(arcGisServer.url + '?f=json', function(error, response) {
             handleResponse(error, response, function(response) {
-              //var max = JSON.parse(response.body).maxRecordCount;
+              var max = JSON.parse(response.body).maxRecordCount;
 
-              queryPaged(500, objectIds, arcGisServer.url + '/query?f=json', function(data) {
+              if (!max) {
+                max = 300;
+              }
+
+              queryPaged(max, objectIds, arcGisServer.url + '/query?f=json', function(data) {
                 //console.log(data);
                 completed++;
               });
